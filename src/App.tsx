ed0b9444e0e8435aba7504f26c5123feac1369c8
Ritik-Patidar <ritikpatidar029@gmail.com>
2024-05-04 22:18:16 +0530

    added api for list

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,28 @@
+import { useEffect, useState } from "react";
 import JobCard from "./components/JobCard";
 
 function App() {
-    const jobData: any = [
-        {
-            jdUid: "cfff35ba-053c-11ef-83d3-06301d0a7178-92012",
-            jdLink: "https://weekday.works",
-            jobDetailsFromCompany:
-                "This is a sample job and you must have displayed it to understand that its not just some random lorem ipsum text but something which was manually written. Oh well, if random text is what you were looking for then here it is: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and now in this assignment.",
-            maxJdSalary: 103,
-            minJdSalary: 100,
-            salaryCurrencyCode: "USD",
-            location: "mumbai",
-            minExp: null,
-            maxExp: null,
-            jobRole: "ios",
-            companyName: "LG",
-            logoUrl: "https://logo.clearbit.com/lg.com",
-        },
-        {
-            jdUid: "cfff35ba-053c-11ef-83d3-06301d0a7178-92012",
-            jdLink: "https://weekday.works",
-            jobDetailsFromCompany:
-                "This is a sample job and you must have displayed it to understand that its not just some random lorem ipsum text but something which was manually written. Oh well, if random text is what you were looking for then here it is: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and now in this assignment.",
-            maxJdSalary: 103,
-            minJdSalary: 100,
-            salaryCurrencyCode: "USD",
-            location: "mumbai",
-            minExp: null,
-            maxExp: null,
-            jobRole: "ios",
-            companyName: "LG",
-            logoUrl: "https://logo.clearbit.com/lg.com",
-        },
-    ];
+    const [jobData, setJobData] = useState([]);
+
+    useEffect(() => {
+        const fetchJobs = async (): Promise<void> => {
+            const response = await fetch(
+                "https://api.weekday.technology/adhoc/getSampleJdJSON",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ offset: 0, limit: 10 }),
+                }
+            );
+            const data = await response.json();
+            console.log("file: App.tsx:189 ~ data:", data.jdList);
+            setJobData((prevPosts: Job[]) => [...prevPosts, ...data.jdList]);
+        };
+        fetchJobs();
+    }, []);
+
     return (
         <>
             {jobData.map((job, index) => (
