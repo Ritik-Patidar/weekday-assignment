6b2b4f779bf8a4684ae61836b55fbfe2ef6e033a
Ritik-Patidar <ritikpatidar029@gmail.com>
2024-05-05 00:30:47 +0530

    added infinity scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,49 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import JobCard from "./components/JobCard";
 
 function App() {
-    const [jobData, setJobData] = useState([]);
+    const [jobData, setJobData] = useState<any>([]);
+    const [page, setPage] = useState<number>(0);
+
+    const observerTarget = useRef<HTMLDivElement>(null);
+
+    const fetchJobs = useCallback(async (): Promise<void> => {
+        const response = await fetch(
+            "https://api.weekday.technology/adhoc/getSampleJdJSON",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ offset: page, limit: 10 }),
+            }
+        );
+        const data = await response.json();
+        console.log("file: App.tsx:189 ~ data:", data.jdList);
+        setJobData((prevPosts: any) => [...prevPosts, ...data.jdList]);
+        setPage((prevPage) => prevPage + 1);
+    }, [page]);
 
     useEffect(() => {
-        const fetchJobs = async (): Promise<void> => {
-            const response = await fetch(
-                "https://api.weekday.technology/adhoc/getSampleJdJSON",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({ offset: 0, limit: 10 }),
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (entries[0].isIntersecting) {
+                    fetchJobs();
                 }
-            );
-            const data = await response.json();
-            console.log("file: App.tsx:189 ~ data:", data.jdList);
-            setJobData((prevPosts: Job[]) => [...prevPosts, ...data.jdList]);
+            },
+            { threshold: 1 }
+        );
+
+        if (observerTarget.current) {
+            observer.observe(observerTarget.current);
+        }
+
+        return () => {
+            if (observerTarget.current) {
+                observer.unobserve(observerTarget.current);
+            }
         };
-        fetchJobs();
-    }, []);
+    }, [fetchJobs, observerTarget]);
 
     return (
         <>
@@ -37,6 +59,7 @@ function App() {
                     minExp={job.minExp}
                 />
             ))}
+            <div className="mb-5" ref={observerTarget}></div>
         </>
     );
 }
