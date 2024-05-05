47bfdc323984a7de9aed0fa74dbc1600ad1ca5b5
Ritik-Patidar <ritikpatidar029@gmail.com>
2024-05-05 09:44:00 +0530

    fix card UI

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,6 +1,14 @@
-import { Modal, Paper, Box, Typography } from "@mui/material";
-import { JobCardProps } from "../types/jobs";
+import {
+    Avatar,
+    Box,
+    Button,
+    Card,
+    CardContent,
+    Modal,
+    Typography,
+} from "@mui/material";
 import { useState } from "react";
+import { JobCardProps } from "../types/jobs";
 
 const JobCard: React.FC<JobCardProps> = ({
     role,
@@ -17,50 +25,95 @@ const JobCard: React.FC<JobCardProps> = ({
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-    const maxDescriptionLength = 100;
-    const truncatedDescription = `${description.slice(
-        0,
-        maxDescriptionLength
-    )}${description.length > maxDescriptionLength ? "..." : ""}`;
     return (
         <>
-            <Paper
-                square={false}
+            <Card
+                sx={{
+                    maxWidth: 360,
+                    overflow: "hidden",
+                }}
+                className="bg-white !rounded-[20px]"
                 elevation={1}
-                className="bg-white !rounded-[20px] p-6"
             >
-                <h2 className="text-xl font-bold mb-2">{role}</h2>
-                <p className="text-gray-600 mb-2">{company}</p>
-                <p className="text-green-600 mb-4">
-                    Estimated Salary: {minSalary} - {maxSalary}{" "}
-                    {salaryCurrencyCode}
-                </p>
-                <p className="text-gray-700 mb-4">
-                    {truncatedDescription}{" "}
-                    {description.length > maxDescriptionLength && (
-                        <button
-                            className="text-blue-500 hover:text-blue-700"
+                <CardContent
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: 2,
+                    }}
+                >
+                    <span className="border rounded-2xl px-2 py-1">
+                        ⏳ Posted 3 days ago
+                    </span>
+                    <Box display="flex" gap={2}>
+                        <img src={logoUrl} alt="logo" className="w-10 h-10" />
+                        <div>
+                            <div className="text-sm font-semibold text-gray-600 mb-1">
+                                <h3 className="MuiBox-root css-rulwqv">
+                                    {company}
+                                </h3>
+                                <h2>{role}</h2>
+                            </div>
+                            <p className="text-xs font-medium mt-1">
+                                {location}
+                            </p>
+                        </div>
+                    </Box>
+                    <Typography variant="body2">
+                        Estimated Salary: {minSalary} - {maxSalary}{" "}
+                        {salaryCurrencyCode} ✅
+                    </Typography>
+                    <Box
+                        sx={{
+                            height: 250,
+                            overflow: "hidden",
+                            maskImage:
+                                "linear-gradient(rgb(255, 255, 255), rgb(255, 255, 255), rgba(255, 255, 255, 0))",
+                        }}
+                    >
+                        <Typography variant="body1">About Company:</Typography>
+                        <Box>{description}</Box>
+                    </Box>
+                    <Box className="relative top-[-20px]">
+                        <Typography
+                            variant="body1"
                             onClick={handleOpen}
+                            className="text-blue-500 hover:text-blue-700 cursor-pointer text-center"
                         >
                             Show more
-                        </button>
-                    )}
-                </p>
-                <div className="flex flex-col gap-2 justify-between">
-                    <div>
-                        <p className="text-gray-500">Minimum Experience</p>
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <p className="text-gray-500"> Minimum Experience</p>
                         <p>{minExp || 0} Years</p>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <button className="bg-purple-600 text-white py-2 px-4 rounded-md">
-                            Easy Apply
-                        </button>
-                        <button className="bg-blue-600 text-white py-2 px-4 rounded-md">
-                            Unlock internal data
-                        </button>
-                    </div>
-                </div>
-            </Paper>
+                    </Box>
+                    <Button variant="contained" color="primary">
+                        ⚡ Easy Apply
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className="flex gap-1"
+                    >
+                        <Avatar
+                            alt="Remy Sharp"
+                            src="https://mui.com/static/images/avatar/1.jpg"
+                            sx={{ width: 24, height: 24 }}
+                        />
+                        <Avatar
+                            alt="Travis Howard"
+                            src="https://mui.com/static/images/avatar/2.jpg"
+                            sx={{ width: 24, height: 24 }}
+                        />
+                        <Avatar
+                            alt="Cindy Baker"
+                            src="https://mui.com/static/images/avatar/3.jpg"
+                            sx={{ width: 24, height: 24 }}
+                        />
+                        Unlock referral aks
+                    </Button>
+                </CardContent>
+            </Card>
             <Modal open={open} onClose={handleClose}>
                 <Box
                     sx={{
